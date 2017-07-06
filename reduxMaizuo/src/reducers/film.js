08f12f9d8031f0f3433f58b6334fb58afb25be02
yyh1945 <yyh1945@126.com>
2017-07-06 10:21:37 +0800

    add:immutable

--- reduxMaizuo/src/reducers/film.js
+++ reduxMaizuo/src/reducers/film.js
@@ -2,32 +2,48 @@
  * Created by zhengguorong on 16/6/28.
  */
 import * as types from '../constants/ActionTypes'
+import Immutable from 'immutable'
 
 const film=(state={},action) => {
+	let obj = null;
   switch (action.type) {
     case types.FETCH_COMING_SOON_SUCCESS:
-      return Object.assign({}, state, {comingSoonFilms:action.comingSoonFilms})
+    	obj = null;
+    	obj = Immutable.fromJS(state);
+    	obj = obj.merge({'comingSoonFilms': action.comingSoonFilms});
+    	return obj.toJS()
+      //return Object.assign({}, state, {comingSoonFilms:action.comingSoonFilms})
     case types.FETCH_NOW_PLAYING_SUCCESS:
-      return Object.assign({}, state, {nowPlayingFilms: {
-      	films: action.nowPlayingFilms.films,
-	      page: action.nowPlayingFilms.page,
-        }
-      })
+	    obj = null;
+    	obj = Immutable.fromJS(state);
+    	obj = obj.merge({nowPlayingFilms: {
+		    films: action.nowPlayingFilms.films,
+		    page: action.nowPlayingFilms.page,
+	    }})
+      return obj.toJS()
 	  case types.FETCH_MORE_NOW_PLAYING_SUCCESS:
 	  	const films = [...state.nowPlayingFilms.films, ...action.nowPlayingFilms.films]
 		  const page = action.nowPlayingFilms.page
-		  if( state.nowPlayingFilms.page.current === state.nowPlayingFilms.page.count
-			  && state.nowPlayingFilms.page.count === page.count ){
+		  if( state.nowPlayingFilms.page.current > page.count ){
 	  		return state;
 		  }
-	  	return Object.assign({}, state, {nowPlayingFilms: {
-	  		films: films,
-		    page: page,
+		  obj = null;
+		  obj = Immutable.fromJS(state);
+		  obj = obj.merge({nowPlayingFilms: {
+			  films: films,
+			  page: page,
 		  } })
+	  	return obj.toJS();
     case types.FETCH_BILLBOARD_SUCCESS:
-      return Object.assign({}, state, {billboards:action.billboards})
+	    obj = null;
+	    obj = Immutable.fromJS(state);
+	    obj = obj.merge({billboards:action.billboards})
+	    return obj.toJS();
     case types.FETCH_DETAIL_SUCCESS:
-      return Object.assign({}, state, {detail:action.detail})
+	    obj = null;
+	    obj = Immutable.fromJS(state);
+	    obj = obj.merge({detail:action.detail})
+	    return obj.toJS();
     default:
       return state
   }
